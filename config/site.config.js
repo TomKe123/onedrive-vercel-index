665c6009cc465311d09a771659bcf2394b4687bd
TomKe <110755646+TomKe123@users.noreply.github.com>
2025-06-19 11:14:50 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -44,7 +44,8 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: ['/微电影项目',
                     '/作文',
-                    '/jokes/素材'
+                    '/jokes/素材',
+                    '/毕业典礼'
                     ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
