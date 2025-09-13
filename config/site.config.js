ad4b8d53216b8d65436987b082d6d149c3adeed0
TomKe <110755646+TomKe123@users.noreply.github.com>
2025-09-13 13:10:52 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,10 +17,10 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Tom Ke's OneDrive",
+  title: "行知中学2028届3班",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/',
+  baseDirectory: '/桌面',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
